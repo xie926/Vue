41691ca6da38b6b2b1b581529e13c0ab73981418
xie926 <1187904163@qq.com>
2019-11-16 11:03:05 +0800

    20191116

---
+++ 3-vueCli/vue-router/src/components/Error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Error',
+  data(){
+    return{
+      msg: 'Error 404'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 3-vueCli/vue-router/src/components/HiWorld2.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HiWorld2',
+  data(){
+    return{
+      msg: 'I am HiWorld2 page'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
