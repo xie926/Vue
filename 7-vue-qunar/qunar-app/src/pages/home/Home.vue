41aa1e0c90d46263f3ae6afe075197f8c38605ed
xie926 <1187904163@qq.com>
2019-11-18 15:36:53 +0800

    去哪儿

---
+++ 7-vue-qunar/qunar-app/src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <home-header></home-header>
+  </div>
+</template>
+
+<script>
+import HomeHeader from "../../components/Home-Head";
+export default {
+  components:{
+    // 'home-header':HomeHeader
+    HomeHeader,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
